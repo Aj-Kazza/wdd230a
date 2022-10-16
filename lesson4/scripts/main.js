128f574b4abb4ed40f6084db8b6a35d4be92e695
Aj-Kazza <kat21008@byui.edu>
2022-10-16 18:58:28 +1200

    lesson 4 chamber home build

---
+++ lesson4/scripts/main.js
@@ -0,0 +1,58 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + ", " + monthName + ", " + d.getDate() + ", " + year;
+let daything = dayName + ", " + d.getDate() + " " + monthName + " " + year;
+
+document.getElementById('daything').textContent = daything;
+
+document.getElementById("currentdate").textContent = fulldate;
+
+document.querySelector("#thisyear").innerHTML = year;
+
+// ******************
+try {
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    };
+    document.getElementById(
+        "currentdate"
+    ).textContent = new Date().toLocaleDateString("en-US", options);
+} catch (e) {
+    alert("Error with code or your browser does not support locale");
+}
+
+function toggleMenu() {
+    document.getElementById('priNav').classList.toggle("open");
+    document.getElementById('burger').classList.toggle("open");
+}
+
+const x = document.getElementById('burger')
+x.onclick = toggleMenu;
