967a3183d8cdbdcf65c4e26c549f64aae16c15d8
Aj-Kazza <kat21008@byui.edu>
2022-11-24 14:32:26 +1200

    progressive loading

---
+++ lesson7/js/script.js
@@ -0,0 +1,51 @@
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+//from video
+/*const imgOptions = {
+    threshold: 0,
+    rootMargin: '0px 0px 0px 0px'
+};
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {});
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}*/
+
+//from mdn
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
