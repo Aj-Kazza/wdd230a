58cd2425d313764bb321b4e7df5df9248735c678
Aj-Kazza <kat21008@byui.edu>
2022-11-26 13:48:19 +1200

    week7 stuff

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,3 +1,4 @@
+//calender stuff
 let daynames = [
     "Sunday",
     "Monday",
@@ -35,6 +36,7 @@ document.getElementById("currentdate").textContent = fulldate;
 
 document.querySelector("#thisyear").innerHTML = year;
 
+
 // ******************
 try {
     let options = {
@@ -50,6 +52,9 @@ try {
     alert("Error with code or your browser does not support locale");
 };
 
+
+
+//burger
 function toggleMenu() {
     document.getElementById('priNav').classList.toggle("open");
     document.getElementById('burger').classList.toggle("open");
@@ -59,10 +64,9 @@ const x = document.getElementById('burger');
 x.onclick = toggleMenu;
 
 
+//anouncement
 const body = document.querySelector('body');
-
 const announcement = document.getElementById('announcement');
-
 
 function meeting() {
     if (dayName == "Monday") {
@@ -78,3 +82,30 @@ function meeting() {
 };
 
 meeting();
+
+
+
+//for visits
+const visitDisplay = document.querySelector("#visits")
+let timestamp1 = window.localStorage.getItem("visit1");
+let timestamp2 = Date.now(window.localStorage.getItem("visit2"));
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+console.log(`number of visits: ${numVisits}`)
+console.log(`timestamp1: ${timestamp1}`)
+if (numVisits !== 0) {
+    localStorage.setItem("visit2", timestamp2)
+    console.log(`timestamp2: ${timestamp2}`)
+    daynum = parseInt(timestamp2) - parseInt(timestamp1)
+    console.log(`time difference: ${daynum}`)
+    daySince = Math.floor(daynum / (24 * 60 * 60 * 1000));
+    console.log(`day since last visit: ${daySince}`)
+    visitDisplay.textContent = daySince;
+    numVisits = 0;
+} else {
+    visitDisplay.textContent = "This is your first visit!";
+    let timestamp1 = Date.now(window.localStorage.getItem("visit1"));
+    localStorage.setItem("visit1", timestamp1);
+}
+
+numVisits++;
+localStorage.setItem("visits-ls", numVisits);
---
+++ chamber/scripts/progressive.js
@@ -0,0 +1,32 @@
+//progressive loading
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+let imgcheck = 0
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+        imgcheck = imgcheck + 1
+        console.log(`Loaded images = ${imgcheck}`)
+
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
